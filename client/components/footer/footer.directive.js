e38b4fe3abaf87f74554f78ddfd95de936cdaa63
franco malatacca <franco@sentinel1a.homenet.telecomitalia.it>
2015-12-07 22:27:17 +0100

    intial commit

---
+++ client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('malaxApp')
+  .directive('footer', function () {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function (scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
